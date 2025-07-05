576187223306c9bf5def52dd86dde692e7eb20b1
nanduladeepak <38109179+nanduladeepak@users.noreply.github.com>
2025-07-05 12:32:41 +0100

    Update TrainModel parameters to enhance training stability and custom column handling

--- ml_model/train.py
+++ ml_model/train.py
@@ -6,11 +6,12 @@ import pandas as pd
 
 model_training_r34 = TrainModel(model_type='resnet18',
                                 epoch = 500, 
-                                stop_percistance= 10, 
+                                stop_percistance= 20, 
                                 att_heads=4, 
                                 poles=5, 
                                 upperCutoff = 45, 
                                 train_custom_cols= True, 
+                                custom_cls_trs= 0.3,
                                 custom_cols = ['MI'])
 model_training_r34.train_model()
 model_training_r34.test_model()
